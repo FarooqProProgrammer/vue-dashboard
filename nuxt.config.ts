070fc86cf11953a97838e299d0e20e4ad39766e7
Muhammad Farooq <farooqayub646@gmail.com>
2025-05-06 01:31:16 -0700

    Add @pinia/nuxt module and update dependencies in package.json and nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     ],
   },
 
-  modules: ['shadcn-nuxt'],
+  modules: ['shadcn-nuxt','@pinia/nuxt'],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
